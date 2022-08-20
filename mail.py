b0cc63ca10904582dd4977a394dbb7cfe9ae8c9a
Yahia AKKAZI <yahyaakkazi@gmail.com>
2022-08-20 02:38:33 +0100

    Update

--- mail.py
+++ mail.py
@@ -2,6 +2,7 @@ from getpass import getpass
 from utils.utils import get_contacts,read_template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 import smtplib
 
 def main(contact = 'mycontacts.txt',message = 'message.txt'):
@@ -9,19 +10,20 @@ def main(contact = 'mycontacts.txt',message = 'message.txt'):
     port = input("ur port as well :) ")
     MY_ADDRESS = str(input("now ur mail address ! "))
     PASSWORD = getpass("Please type your mail password, no worries it will be invisible")
-    names, emails = get_contacts('mycontacts.txt') # read contacts
+    civs, names, comps, emails  = get_contacts('mycontacts.txt') # read contacts
     message_template = read_template('message.txt')
     # set up the SMTP server
     s = smtplib.SMTP(host=host, port=port)
     s.starttls()
     s.login(MY_ADDRESS, PASSWORD)
+    files = ["data/AKKAZI_Yahia_Certifications.pdf", "data/AKKAZI_Yahia_Resume_EN.pdf", "data/AKKAZI_Yahia_Resume_FR.pdf"]
 
     # For each contact, send the email:
-    for name, email in zip(names, emails):
+    for civ, name, comp, email in zip(civs, names, comps, emails):
         msg = MIMEMultipart()       # create a message
 
         # add in the actual person name to the message template
-        message = message_template.substitute(PERSON_NAME=name.title())
+        message = message_template.substitute(PERSON_NAME=name.title(),COMPANY = comp.title(),CIV = civ.title())
 
         # Prints out the message body for our sake
         print(message)
@@ -29,11 +31,14 @@ def main(contact = 'mycontacts.txt',message = 'message.txt'):
         # setup the parameters of the message
         msg['From']=MY_ADDRESS
         msg['To']=email
-        msg['Subject']="This is TEST"
+        msg['Subject']="Candidature spontanée pour un stage en Data"
         
         # add in the message body
         msg.attach(MIMEText(message, 'plain'))
-        
+        for f in files:  # add files to the message
+            attachment = MIMEApplication(open(f, "rb").read(), _subtype="txt")
+            attachment.add_header('Content-Disposition','attachment', filename=f)
+            msg.attach(attachment)
         # send the message via the server set up earlier.
         s.send_message(msg)
         print("done")
--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,17 @@
 from string import Template
 
 def get_contacts(filename):
+    civs = []
+    comps = []
     names = []
     emails = []
     with open(filename, mode='r', encoding='utf-8') as contacts_file:
         for a_contact in contacts_file:
-            names.append(a_contact.split()[0])
-            emails.append(a_contact.split()[1])
-    return names, emails
+            civs.append(a_contact.split()[0])
+            names.append(a_contact.split()[1])
+            comps.append(a_contact.split()[2])
+            emails.append(a_contact.split()[3])
+    return civs,names,comps, emails
 
 
 def read_template(filename):
